a54c75863f81ab97ba727eba9a3a3b50a918ff74
Guy Michalovitz <guym@landa-corp.com>
2023-04-20 16:45:03 +0300

    commit

--- my_set/my_set.c
+++ my_set/my_set.c
@@ -51,7 +51,7 @@ struct Set get_set(struct Set *all_nums)
     {
         scanf("%d", &num);
         isNumFound = 0;
-        resize_set(&all_nums);
+        resize_set(all_nums);
         all_nums->numbers[all_nums->length - 1] = num;
 
         for (i = 0; i < set.length; i++)
